8bc18e5ca598e81e56339d45ed91506afbed1674
Coding94 <federicolinus@hotmail.com>
2025-10-08 11:42:56 -0600

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,19 @@
+// Define the unique cards
+const cardsArray = [
+  { name: "buffalo", img: "images/buffalo.png" },
+  { name: "crocodile-2", img: "images/crocodile-2.png" },
+  { name: "crocodile", img: "images/crocodile.png" },
+  { name: "rabbit", img: "images/rabbit.png" },
+  { name: "cow", img: "images/cow.png" },
+  { name: "flamingo", img: "images/flamingo.png" },
+  { name: "fox", img: "images/fox.png" },
+  { name: "giraffe-2", img: "images/giraffe-2.png" },
+  { name: "giraffe", img: "images/giraffe.png" },
+  { name: "horse", img: "images/horse.png" },
+  { name: "pig", img: "images/pig.png" },
+  { name: "hedgehog", img: "images/hedgehog.png" },
+  { name: "lion", img: "images/lion.png" },
+  { name: "hippo", img: "images/hippo.png" },
+  { name: "sheep", img: "images/sheep.png" }
+];
+
